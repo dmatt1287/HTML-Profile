2d7292937e2e4a45d1f89ef760501d36ce0b57de
dmatt1287 <131632506+dmatt1287@users.noreply.github.com>
2024-06-23 14:56:20 -0500

    added login page template

---
+++ loginPage/js.js
@@ -0,0 +1,12 @@
+<script >
+    $(".nav-header-links a").hover(function () {
+        $(this).css("opacity", "1.0");
+    }, function () {
+        $(this).css("opacity", "0.6");
+    });
+$("#log-btn").hover(function () {
+    $(this).css("opacity", "1.0");
+}, function () {
+    $(this).css("opacity", "0.9");
+});
+</script >
